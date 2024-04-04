e2f456641a2f576f82f4c61cb8d4fdc1641f5832
zhuzhanji <37281560+zhuzhanji@users.noreply.github.com>
2024-04-04 14:20:39 +0100

    Update clustalw.py

--- scripts/clustalw.py
+++ scripts/clustalw.py
@@ -3,16 +3,18 @@ import utils as utl
 import config as cg
 
 if __name__ == "__main__":
+  # pre-align GI
+  utl.Clustalw(sequences = cg.paths['prefix']['GI'][0] + '.orf', output = cg.paths['prefix']['GI'][0] + '.aln')
+  utl.Clustalw(sequences = cg.paths['prefix']['GI'][1] + '.orf', output = cg.paths['prefix']['GI'][1] + '.aln')
   
-  #utl.Clustalw(sequences = cg.paths['prefix']['GI'][0] + '.orf', output = cg.paths['prefix']['GI'][0] + '.aln')
-  #utl.Clustalw(sequences = cg.paths['prefix']['GI'][1] + '.pro', output = cg.paths['prefix']['GI'][1] + '.aln')
-
-  #path2 = cg.paths['prefix']['GII'][0]
-  #utl.Clustalw(sequences = path2 + '.orf', output = path2 + '.aln')
-  #path2 = cg.paths['prefix']['GII'][1]
-  #utl.Clustalw(sequences = path2 + '.pro', output = path2 + '.aln')
-
+  # pre-align GII
+  path2 = cg.paths['prefix']['GII'][0]
+  utl.Clustalw(sequences = path2 + '.orf', output = path2 + '.aln')
+  path2 = cg.paths['prefix']['GII'][1]
+  utl.Clustalw(sequences = path2 + '.orf', output = path2 + '.aln')
+  
+  # pre-align all orf1, pre-align all orf2
   path2 = "./../new_data/reference_sequences_RdRp/all"
   utl.Clustalw(sequences = path2 + '.orf', output = path2 + '.aln')
   path2 = "./../new_data/reference_sequences_VP1/all"
-  utl.Clustalw(sequences = path2 + '.pro', output = path2 + '.aln')
+  utl.Clustalw(sequences = path2 + '.orf', output = path2 + '.aln')
